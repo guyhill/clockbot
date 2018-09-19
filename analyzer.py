4b12a1d5a33e1b65af8f1c075d5f9a5282c75612
Guido van den Heuvel <ehvl.cbs@gmail.com>
2018-09-19 18:03:48 +0200

    First release

--- analyzer.py
+++ analyzer.py
@@ -1,5 +1,13 @@
 import csv
+import sys
+import os
 from time import strftime, localtime
+
+if len(sys.argv) < 2:
+    print("Missing argument: input filename")
+    exit(1)
+in_filename = sys.argv[1]
+out_filename_prefix = os.path.splitext(in_filename)[0]
 
 csv.register_dialect("nl_excel", csv.excel, delimiter = ";")
 f_out = None
@@ -7,25 +15,32 @@ f_out = None
 def match_prefix(s, prefix):
     return s[:len(prefix)] == prefix, s[len(prefix):]
 
-with open("results.txt", "r") as f_in:
+with open(in_filename, "r") as f_in:
     for line in f_in.readlines():
         timestamp = float(line[24:41])
         message = line[42:].strip()
         is_match, suffix = match_prefix(message, "Filling in username: ")
         if is_match:
-            filename = strftime("%Y%m%d_%H%M%S", localtime(timestamp)) + "_" + suffix + ".csv"
+            filename = out_filename_prefix + "_" + suffix + ".csv"
             if f_out is not None:
                 f_out.close()
             f_out = open(filename, "w", newline = "")
             csv_out = csv.writer(f_out, dialect = "nl_excel")
-            csv_out.writerow(["current_item", "next_item", "elapsed_time"])
-            current_item = None
-            current_timestamp = timestamp
+            csv_out.writerow(["time_since_login", "time_since_previous", "item"])
+        is_match, _ = match_prefix(message, "Pressing login button")
+        if is_match:
+            prev_item = None
+            prev_timestamp = None
+            start_timestamp = timestamp
         is_match, suffix = match_prefix(message, "Active item: ")
         if is_match:
             next_item = suffix
             next_timestamp = timestamp
-            if current_item is not None:
-                csv_out.writerow([current_item, next_item, next_timestamp - current_timestamp])
-            current_item = next_item
-            current_timestamp = next_timestamp
+            csv_out.writerow([
+                "{:.3f}".format(next_timestamp - start_timestamp), 
+                "{:.3f}".format(next_timestamp - prev_timestamp) if prev_timestamp is not None else None, 
+                next_item
+            ])
+            prev_timestamp = next_timestamp
+
+        
