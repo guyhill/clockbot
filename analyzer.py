45742f81c44f3fb7a0dfdffabd2a5db73f9b2ef5
Guido van den Heuvel <guyhill@xs4all.nl>
2018-09-22 20:58:41 +0200

    analyzer.py now fully object oriented

--- analyzer.py
+++ analyzer.py
@@ -1,75 +1,101 @@
 import csv
 import sys
 import os
-from time import strftime, localtime
-
-def match_prefix(s, prefix):
-    return s[:len(prefix)] == prefix, s[len(prefix):]
-
-action_lines = [
-    "Starting browser: ",
-    "Traversal method: ",
-    "Filling in username: ",
-    "Pressing login button",
-    "Navigating to next menu item",
-    "Navigating to menu item",
-    "Active item: ",
-    "Not on first page",
-    "Closing browser",
-    "Login complete",
-    "Navigation complete"
-]
-
-if len(sys.argv) < 2:
-    print("Missing argument: input filename")
-    exit(1)
-in_filename = sys.argv[1]
-out_filename_prefix = os.path.splitext(in_filename)[0]
 
 csv.register_dialect("nl_excel", csv.excel, delimiter = ";")
-f_out = open(out_filename_prefix + "_clean.csv", "w", newline = "", encoding = "utf-8")
-csv_out = csv.writer(f_out, dialect = "nl_excel")
-csv_out.writerow(["browser", "methode", "run_nr", "gebruikersnaam", "laadtijd", "item"])
 
-runs = {}
-buffer = []
+class ClockbotLogMiner:
 
-silent = False
-browser = None
-traversal_method = None
-username = None
-start_time = None
-end_time = None
-item = None
-with open(in_filename, "r", encoding = "utf-8") as f_in:
-    for line in f_in.readlines():
-        timestamp = float(line[24:41])
-        message = line[42:].strip()
-        for action_line in action_lines:
-            is_match, suffix = match_prefix(message, action_line)
-            if is_match:
-                if action_line == action_lines[0]:
-                    browser = suffix
-                elif action_line == action_lines[1]:
-                    traversal_method = suffix
-                elif action_line == action_lines[2]:
-                    username = suffix
-                    if username not in runs:
-                        runs[username] = 1
-                elif action_line == action_lines[3] or action_line == action_lines[4] or action_line == action_lines[5]:
-                    start_time = timestamp
-                elif action_line == action_lines[6]:
-                    item = suffix
-                    buffer.append([browser, traversal_method, runs[username], username, "{:.3f}".format(end_time - start_time).replace(".", ","), item])
-                elif action_line == action_lines[7]:
-                    silent = True
-                elif action_line == action_lines[8]:
-                    if not silent:
-                        for output in buffer:
-                            csv_out.writerow(output)
-                        runs[username] += 1
-                    buffer = []
-                    silent = False
-                elif action_line == action_lines[9] or action_line == action_lines[10]:
-                    end_time = timestamp
-                break
+    def __init__(self, filename):
+        self.f_in = open(filename, "r", encoding = "utf-8")
+        
+        out_filename = os.path.splitext(in_filename)[0] + "_timedata.csv"
+        self.f_out = open(out_filename, "w", newline = "", encoding = "utf-8")
+        self.csv_out = csv.writer(self.f_out, dialect = "nl_excel")
+        self.csv_out.writerow(["browser", "methode", "run_nr", "gebruikersnaam", "laadtijd", "item"])
+
+        self.runs = {}
+        self.buffer = []
+
+        self.silent = False
+        self.browser = None
+        self.traversal_method = None
+        self.username = None
+        self.start_time = None
+        self.end_time = None
+        self.item = None
+
+    def process_all(self):
+        for line in self.f_in.readlines():
+            timestamp = float(line[24:41])
+            message = line[42:].strip()
+            
+            for action_line, handler in self.action_lines:
+                is_match = message[:len(action_line)] == action_line
+                suffix = message[len(action_line):]
+                if is_match:
+                    handler(self, timestamp, suffix)
+                    break
+
+    def set_browser_name(self, t, browser_name):
+        self.browser = browser_name
+
+    def set_traversal_method(self, t, traversal_method):
+        self.traversal_method = traversal_method
+
+    def set_username(self, t, username):
+        self.username = username
+        if self.username not in self.runs:
+            self.runs[self.username] = 1
+
+    def set_start_time(self, timestamp, s):
+        self.start_time = timestamp
+
+    def set_end_time(self, timestamp, s):
+        self.end_time = timestamp
+
+    def set_item(self, t, item):
+        self.item = item
+        self.buffer.append([
+            self.browser, 
+            self.traversal_method, 
+            self.runs[self.username], 
+            self.username, 
+            "{:.3f}".format(self.end_time - self.start_time).replace(".", ","), 
+            self.item
+        ])
+
+    def set_silent(self, t, s):
+        self.silent = True
+
+    def write_data(self, t, s):
+        if not self.silent:
+            for output in self.buffer:
+                self.csv_out.writerow(output)
+            self.runs[self.username] += 1
+        self.buffer = []
+        self.silent = False
+
+    action_lines = [
+        ("Starting browser: ", set_browser_name),
+        ("Traversal method: ", set_traversal_method),
+        ("Filling in username: ", set_username),
+        ("Pressing login button", set_start_time),
+        ("Navigating to first menu item", set_start_time),
+        ("Navigating to next menu item", set_start_time),
+        ("Navigating to menu item", set_start_time),
+        ("Active item: ", set_item),
+        ("Not on first page", set_silent),
+        ("Closing browser", write_data),
+        ("Login complete", set_end_time),
+        ("Navigation complete", set_end_time)
+    ]
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Missing argument: input filename")
+        exit(1)
+    in_filename = sys.argv[1]
+
+    clm = ClockbotLogMiner(in_filename)
+    clm.process_all()
