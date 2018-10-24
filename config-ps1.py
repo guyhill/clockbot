09015535d23f695da2aa97d889156d2829dba5ab
Guido van den Heuvel <ehvl.cbs@gmail.com>
2018-10-24 11:40:27 +0200

    edit

---
+++ config-ps1.py
@@ -0,0 +1,18 @@
+# selectors for all items we are interested in
+# Logging in
+css_username_field = "input#Username"
+css_password_field = "input#Password"
+css_login_button = "input[value='Inloggen']"
+
+# Wait for page load
+css_wait_full_page = "#x"
+
+# Deal with menu items
+css_menu_id = "#v"
+css_menu_items = css_menu_id + " " + "a"
+active_font_classname = "Font12"
+css_menu_active_font = "." + active_font_classname
+css_menu_active_item = css_menu_id + " " + css_menu_active_font
+
+# Next page button
+css_next_page_button = "#ac"
